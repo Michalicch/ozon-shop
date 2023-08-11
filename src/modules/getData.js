384e897dddecbb2e2264d3abaac179fc8ee0ece7
Michalicch <lorka.diskaveri@gmail.com>
2023-08-11 23:01:36 +0300

    День 2. Получение данных из файлов БД

---
+++ src/modules/getData.js
@@ -0,0 +1,9 @@
+const getData = (str) => {
+	return fetch('https://ozon-shop-e9c48-default-rtdb.firebaseio.com/goods.json')
+		.then((response) => {
+			return response.json()
+		})
+
+}
+
+export default getData;
