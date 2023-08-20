41fba2d7e6cb3b5a464fb8ba9945a2a643f9f497
Michalicch <lorka.diskaveri@gmail.com>
2023-08-20 17:04:09 +0300

    Сделан фильтр по цене и скидке

--- src/modules/filters.js
+++ src/modules/filters.js
@@ -11,3 +11,15 @@ export const categoryFilter = (goods, value) => {
 		return goodsItem.category === value
 	})
 }
+
+export const priceFilter = (goods, minValue, maxValue, sale) => {
+	const checkSale = document.getElementById('discount-checkbox')
+	
+	return goods.filter((goodsItem) => {
+		const isMin = minValue.trim() ? goodsItem.price >= parseInt(minValue.trim()) : true
+		const isMax = maxValue.trim() ? goodsItem.price <= parseInt(maxValue.trim()) : true
+		const isSale = sale ? goodsItem.sale : true
+		
+		return !checkSale.checked ? isMin && isMax : isMin && isMax && isSale		
+	})
+}
--- src/modules/search.js
+++ src/modules/search.js
@@ -2,17 +2,42 @@ import getData from "./getData"
 import renderGoods from "./renderGoods"
 import { searchFilter } from "./filters"
 import { debounce } from "./helpers"
+import { priceFilter } from "./filters"
 
 const search = () => {
 	const searchInput = document.querySelector('.search-wrapper_input')
+	const minInp = document.getElementById('min')
+	const maxInp = document.getElementById('max')
+	const checkSale = document.getElementById('discount-checkbox')
+	const checkMark = document.querySelector('.filter-check_checkmark')
 
 	const debounceSearch = debounce((event)=>{
 		getData().then((data) => {
 			renderGoods(searchFilter(data, event.target.value));
 		})
 	}, 2000)	
+//, sale = false
+	const debouncePrice = debounce((min = '', max = '', sale = false)=>{
+		getData().then((data => {
+			renderGoods(priceFilter(data, min, max, sale));
+		}))
+	})	
 	
 	searchInput.addEventListener('input', debounceSearch)	
+
+	minInp.addEventListener('input', (event) =>{
+		debouncePrice(minInp.value, maxInp.value, checkSale.value)
+	})
+	maxInp.addEventListener('input', (event) =>{
+		debouncePrice(minInp.value, maxInp.value, checkSale.value)
+	})
+
+	checkSale.addEventListener('change', ()=>{		
+		checkMark.classList.toggle('checked')		
+		debouncePrice(minInp.value, maxInp.value, checkSale.checked)
+	})
+
+
 }
 
 export default search
