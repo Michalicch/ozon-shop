beafe38bc535a4580f4accd4cd3a3f97ff85c620
Michalicch <lorka.diskaveri@gmail.com>
2023-08-20 11:22:52 +0300

    добавлена функция debounce - отложенный запуск функции поиска

--- src/modules/search.js
+++ src/modules/search.js
@@ -1,24 +1,18 @@
 import getData from "./getData"
 import renderGoods from "./renderGoods"
 import { searchFilter } from "./filters"
+import { debounce } from "./helpers"
 
 const search = () => {
 	const searchInput = document.querySelector('.search-wrapper_input')
 
-	searchInput.addEventListener('input', (event) => {
-		const value = event.target.value
-
+	const debounceSearch = debounce((event)=>{
 		getData().then((data) => {
-			renderGoods(searchFilter(data, value));
+			renderGoods(searchFilter(data, event.target.value));
 		})
-	})
-
-	// const debouceSearch = debounce((event) => {
-	// 	getData().then((data) => {
-	// 		renderGoods(searchFilter(data, event.target.value));
-	// 	})
-	// }, 2000)
-	// searchInput.addEventListener('input', debouceSearch)
+	}, 2000)	
+	
+	searchInput.addEventListener('input', debounceSearch)	
 }
 
 export default search
