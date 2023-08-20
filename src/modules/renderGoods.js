ec370c447b8fef066ccc834c42cb73eb4e6f5901
Michalicch <lorka.diskaveri@gmail.com>
2023-08-20 22:13:19 +0300

    Корзина, добавление / удаление товаров. подсчет суммы заказа. Отправка заказа

--- src/modules/renderGoods.js
+++ src/modules/renderGoods.js
@@ -1,12 +1,14 @@
 const renderGoods = (goods) => {
 	const goodsWrapper = document.querySelector('.goods') //контейнер карточек товаров
 
+	localStorage.setItem('goods', JSON.stringify(goods))
+
 	goodsWrapper.innerHTML = '' //очищаем чтобы верстка не дублировалась
 
 	goods.forEach(goodsItem => {
 		goodsWrapper.insertAdjacentHTML('beforeend', `
 			<div class="col-12 col-md-6 col-lg-4 col-xl-3">
-				<div class="card">
+				<div class="card" data-key="${goodsItem.id}">
 					${goodsItem.sale ? '<div class="card-sale">🔥Hot Sale🔥</div>' : ''}
 					<div class="card-img-wrapper">
 						<span class="card-img-top"
